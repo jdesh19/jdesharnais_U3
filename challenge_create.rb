9c6355af00f7011586b0fa36ae5db09bb80b9bfa
jdesh19 <jarondesharnais@gmail.com>
2024-05-27 22:34:37 +0000

    Challenge_create.rb is done

---
+++ challenge_create.rb
@@ -0,0 +1,32 @@
+require_relative 'ar.rb'
+#1
+new_product = Product.new
+
+new_product.name = "Microphone"
+new_product.description = "Speak into it"
+new_product.price = 30
+new_product.stock_quantity = 50
+
+new_product.save
+puts new_product.inspect
+puts "\n"
+#2
+headphones = Product.new(
+                            name: "Headphones",
+                            description: "Use to listen to things on your computer",
+                            price: 300,
+                            stock_quantity: 20
+                        )
+
+headphones.save
+puts headphones.inspect
+
+#3
+monitor = Product.create(
+                            name: "Monitor",
+                            description: "Use to see things on your computer",
+                            price: 400,
+                            stock_quantity: 10
+                        )
+
+puts monitor.inspect
--- models/product.rb
+++ models/product.rb
@@ -4,17 +4,20 @@ class Product < ActiveRecord::Base
   # A product has a many to one relationship with a category.
   # The products table has a category_id foreign key.
   # In other words, a product belongs to a category.
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :description, :price, :stock_quantity, presence: true
   belongs_to :category
 end
 
 =begin
-Product Table Columns
+Product Table Columns:
+
 id
 name
 description
 price
 stock_quantity
-category_id
+category_id Relation to category table.
 created_at
 updated_at
 =end
